8728a5fbf966f3a860904cb4f08d274596f5eab9
thanawat <>
2019-03-28 18:17:53 +0700

    layout- laser id

---
+++ pointpow/Popup/PopUpInfoLaserIdViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PopUpInfoLaserIdViewController.swift
+//  pointpow
+//
+//  Created by thanawat on 28/3/2562 BE.
+//  Copyright © 2562 abcpoint. All rights reserved.
+//
+
+import UIKit
+
+class PopUpInfoLaserIdViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    override func dismissPoPup() {
+        super.dismissPoPup()
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.addCloseBlackView()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.dismissPoPup()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
